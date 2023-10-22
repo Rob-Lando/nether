507c15706dad5320f10f86168b6c03ca944ab957
Rob-Lando <Roblando95@outlook.com>
2023-10-22 19:27:28 -0400

    add eth_getTransactionCount method

--- methods/state.nim
+++ methods/state.nim
@@ -44,7 +44,48 @@ proc eth_getBalance(
             "id":id
         }
 
-        let response = postContent(client = client, url = url, body = $data)
-        let responseJson = parseJson(response)
+        let 
+            response = postContent(client = client, url = url, body = $data)
+            responseJson = parseJson(response)
+
+        return responseJson
+
+
+proc eth_getTransactionCount(
+            provider: Provider,
+            address: string,
+            block_number: int = -1,
+            default_block: string = "latest",
+            id: int = 1,
+            headers: HttpHeaders = newHttpHeaders({"Content-Type":"application/json"})
+        ): JsonNode =
+
+        var 
+            client = newHttpClient()
+            params: array[2,string]
+            url: string
+        
+        client.headers = headers
+
+        if provider.url != "":
+            url = provider.url
+        else:
+            url = fmt"{provider.base_url}/{provider.api_key}"
+
+        if default_block != "":
+            params = [address,default_block]
+        else:
+            params = [address,fmt"{block_number}"]
+
+        let data = %*{
+            "jsonrpc":"2.0",
+            "method":"eth_getTransactionCount",
+            "params": params,
+            "id":id
+        }
+
+        let 
+            response = postContent(client = client, url = url, body = $data)
+            responseJson = parseJson(response)
 
         return responseJson
